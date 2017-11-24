e7153a1afe5fc8140254c5c2df1e4ad4ee5f55ac
UMeowBing <547897168@qq.com>
2017-11-24 15:17:31 +0800

    Create [easy]-566-Reshape the Matrix.py

---
+++ [easy]-566-Reshape the Matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        
+        L = []
+        num = sum(nums,[])
+        if r*c != len(num):return nums
+        else:
+            [m,n] = [0,c]
+            for i in range(r):
+                L.append(num[m:n])
+                m,n = m + c,n + c
+            return L
+                      
+        
+                        

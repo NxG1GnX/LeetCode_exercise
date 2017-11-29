a7f0a4ffa9212a080800a9c5a65e288ca454bee5
UMeowBing <547897168@qq.com>
2017-11-29 12:18:23 +0800

    Update [easy]-448-Find All Numbers Disappeared in an Array.py

--- easy/[easy]-448-Find All Numbers Disappeared in an Array.py
+++ easy/[easy]-448-Find All Numbers Disappeared in an Array.py
@@ -4,6 +4,9 @@ class Solution:
         :type nums: List[int]
         :rtype: List[int]
         """
+        # 正负号标记法，将元素和索引联系起来
+        # python中enumerate（）的用法
+        
         for i in nums:
             nums[abs(i)-1] = -abs(nums[abs(i)-1])
         
